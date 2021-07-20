76f57c3d587906ddc875d350d6892e710b376903
luca-jose <lucasjose.pe.21@gmail.com>
2021-07-20 16:39:45 -0300

    Correção id

--- src/main/java/com/accenture/AcademiaSpringBoot/repository/AlunoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.AcademiaSpringBoot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.accenture.AcademiaSpringBoot.model.Aluno;
-
-public interface AlunoRepository extends JpaRepository <Aluno, String >{
-
-}
--- src/main/java/com/accenture/AcademiaSpringBoot/repository/ProfessorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.accenture.AcademiaSpringBoot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.accenture.AcademiaSpringBoot.model.Professor;
-
-public interface ProfessorRepository extends JpaRepository <Professor, String >{
-
-}
